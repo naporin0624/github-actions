a2e46237727948d56fadc8882159ac3de3fece83
napori0624 <naporin0624@gmail.com>
2020-04-14 00:33:28 +0900

    :+1:

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world")
+// import os from "@actions/core";
+import * as puppeteer from "puppeteer-core";
+// import io from "@actions/io";
+
+const BROWSER_PATH = "/usr/bin/google-chrome";
+
+async function main() {
+  const browser = await puppeteer.launch({
+    executablePath: BROWSER_PATH
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com", { waitUntil: "networkidle2" });
+  await page.waitFor(3000);
+  await page.screenshot({fullPage: true, path: `${process.env.GITHUB_WORKSPACE}/screenshots/screenshot.png`});
+  await browser.close();
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
