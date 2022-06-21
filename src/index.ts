3a0d9c13ee846d374d7fb9b320f167a4cb15bc31
naporin0624 <naporin0624@gmail.com>
2022-06-21 10:53:27 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const main = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/2")
   console.log(await response.json())
 }
   
