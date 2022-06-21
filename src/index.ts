1ef7fadb19c4a7e3109ec4b8d64b41dcf88cd292
naporin0624 <naporin0624@gmail.com>
2022-06-21 10:08:25 +0900

    :+1: build and run

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import axios from "axios"
+import fetch from "node-fetch";
 
 const main = async () => {
-  const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
-  console.log(data)
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  console.log(await response.json())
 }
   
 main()
