867e3f7e516b90c781d6f07fa3b7dd325a6b65fb
naporin0624 <naporin0624@gmail.com>
2022-06-21 10:09:32 +0900

    :+1:

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import fetch from "node-fetch";
+import axios from "axios"
 
 const main = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-  console.log(await response.json())
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+  console.log(data)
 }
-  
+
 main()
